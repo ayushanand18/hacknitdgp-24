959a0517711d9975a34009f3f3338cfacfc023e2
ayush anand <36472216+ayushanand18@users.noreply.github.com>
2024-02-23 23:45:08 +0530

    feat: adds RightArrow Component + Tracks Full Page

---
+++ components/RightArrow.tsx
@@ -0,0 +1,20 @@
+const RightArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="w-6 h-6 relative bottom-[-5px]"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M9 5l7 7-7 7"
+      />
+    </svg>
+  );
+};
+
+export default RightArrow;
