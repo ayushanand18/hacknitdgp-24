55d7681b99ac2b96a6f92dd0d9d169225131c47c
ayush anand <36472216+ayushanand18@users.noreply.github.com>
2024-03-05 01:32:30 +0530

    feat: new look to website

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import RightArrow from "@/components/RightArrow";
 import { motion } from "framer-motion";
 import * as React from "react";
 import Image from "next/image";
-
+import { MapPin } from "lucide-react";
 import ETHLogo from "@/assets/ethindia-light.png";
 import DevfolioLogo from "@/assets/Devfolio_Logo-White.png";
 import PolygonLogo from "@/assets/Polygon_Logo-White@2x.png";
+import BannerImage from "@/assets/main banner.jpg";
+import IsteLogo from "@/assets/white iste logo.png";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   React.useEffect(() => {
@@ -19,26 +23,52 @@ export default function Home() {
       document.body.removeChild(script);
     };
   }, []);
+
+  const prizes = [
+    {title: 'Winner', subtitle: 'All Tracks', description: 'FREE Domain Name for an year. Host your website on your custom domain using a GoDaddy Domain.'},
+    {title: 'Best hack built on Polygon: $200', subtitle: 'Web3 Track', description: 'Read about the bounty details and find code templates for Polygon here: https://nsb.dev/polygon-bounty.'},
+    {title: 'Best hack built on Ethereum: $100', subtitle: 'Web3 Track', description: "Select this track if you're building on Ethereum to be eligible for the prizes!"},
+  ]
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-[url('https://assets.squarespace.com/universal/images-v6/parking-page/black-grid-1500w.avif')]">
-      <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="md:fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-2 md:pb-6 md:pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          NITDGP Hacks 1.0
-        </p>
-        <div className="md:fixed bottom-0 left-0 flex h-12 md:h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-0 md:p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.istenitdgp.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By
-            <div className="font-bold">
-              ISTE Students&apos; Chapter NIT Durgapur
-            </div>
+      <nav className="rounded-lg w-full bg-opacity-50 bg-black text-white py-4 px-8 flex justify-between items-center">
+        <Image
+          alt="Logo"
+          className="h-12"
+          height="50"
+          src={IsteLogo}
+          style={{
+            aspectRatio: "50/50",
+            objectFit: "cover",
+          }}
+          width="50"
+        />
+        <div className="hidden md:flex items-center space-x-4 lg:space-x-8">
+          <a className="hover:text-blue-500" href="/">
+            Home
           </a>
+          <a className="hover:text-blue-500" href="/tracks">
+            Tracks
+          </a>
+          <a
+            className="hover:text-blue-500"
+            href="https://www.istenitdgp.com/events"
+          >
+            Events
+          </a>
+          <a
+            className="hover:text-blue-500"
+            href="https://www.istenitdgp.com/teams"
+          >
+            Team
+          </a>
+          <Link href="https://nitdgp-hacks.devfolio.co/">
+            <Button className="bg-blue-500 hover:bg-blue-600 text-white">
+              Register
+            </Button>
+          </Link>
         </div>
-      </div>
+      </nav>
 
       <motion.div
         initial={{ opacity: 0, y: 40 }}
@@ -48,7 +78,7 @@ export default function Home() {
           duration: 0.95,
           ease: [0.165, 0.84, 0.44, 1],
         }}
-        className="flex flex-col z-10 max-w-6xl w-full items-start justify-between text-sm lg:flex my-8"
+        className="flex flex-col z-10 max-w-6xl w-full items-center text-center justify-between text-sm lg:flex my-8"
       >
         <p className="md:fixed left-0 top-0 font-bold text-white text-7xl md:text-8xl max-w-2xl justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur dark:border-neutral-800 lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4">
           Innovate For{" "}
@@ -58,12 +88,11 @@ export default function Home() {
         </p>
         <p className="md:fixed left-0 top-0 flex font-normal text-white text-lg max-w-2xl justify-center border-b border-gray-300 pb-6 pt-8 dark:border-neutral-800 lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4">
           Join the innovation spree for a better future this March with NITDGP
-          Hacks 1.0 2024, one of Eastern India&apos;s largest Hackathon.
+          Hacks 1.0, one of Eastern India&apos;s largest Hackathon.
         </p>
 
-        <p className="md:fixed left-0 top-0 flex font-bold gap-4 text-white text-lg max-w-2xl justify-center border-b border-gray-300 dark:border-neutral-800 lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4">
-          <span className="font-light">9-10th March 2024</span>
-          <span className="">
+        <p className="md:fixed left-0 top-0 flex flex-col items-center font-bold text-white text-lg max-w-2xl justify-center border-b border-gray-300 dark:border-neutral-800 lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4">
+          <span className="py-4">
             <div
               className="apply-button"
               data-hackathon-slug="nitdgp-hacks"
@@ -71,28 +100,69 @@ export default function Home() {
               style={{ height: "44px", width: "312px" }}
             ></div>
           </span>
+
+          <span className="text-2xl font-bold">
+            9 - 10<sup>th</sup> March 2024
+          </span>
+
+          <div className="flex flex-row">
+            <MapPin className="h-8 w-8 my-2" />
+            <div className="flex flex-col">
+              <span className="text-lg font-light text-center  max-w-[12em]">
+                National Institute of Technology Durgapur
+              </span>
+            </div>
+          </div>
         </p>
       </motion.div>
 
+      <div className="w-full px-4 items-center flex flex-col flex-wrap">
+        <Image src={BannerImage} height="300" width="800" alt="hackathon banner"/>
+      </div>
       <div className="flex flex-col w-full p-10 justify-center gap-12">
-        <h3 className="text-whte text-5xl font-black">Sponsors</h3>
+        <h3 className="text-whte text-4xl font-black">Sponsors</h3>
         <div className="flex flex-row gap-6">
           <Image
             height="100"
             alt="DEVFOLIO LOGO"
             width="320"
+            className="p-4"
             src={DevfolioLogo}
           />
           <Image
             height="140"
             alt="POLYGON LOGO"
             width="300"
+            className="p-4"
             src={PolygonLogo}
           />
-          <Image height="140" alt="ETHINDIA LOGO" width="300" src={ETHLogo} />
+          <Image
+            height="140"
+            alt="ETHINDIA LOGO"
+            className="p-4"
+            width="300"
+            src={ETHLogo}
+          />
         </div>
       </div>
 
+      <div className="flex flex-col w-full p-10 justify-center gap-12">
+        <h3 className="text-whte text-4xl font-black">Prizes</h3>
+        <div className="flex flex-col space-y-6">
+          {prizes.map((prize, index) => (
+            <div className="flex items-center space-x-4 ">
+            <div className="w-1 h-36 bg-gradient-to-b from-purple-500 to-pink-500" />
+            <div className="gap-y-2 flex flex-col">
+              <h3 className="text-2xl py-2 font-semibold underline underline-offset-8">{prize.title}</h3>
+              <h4 className="text-lg text-slate-200">{prize.subtitle}</h4>
+              <p className="text-sm text-slate-200 max-w-md">
+                {prize.description}
+              </p>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
       <motion.div
         initial={{ opacity: 0, x: -40 }}
         animate={{ opacity: 1, x: 0 }}
@@ -104,7 +174,7 @@ export default function Home() {
         className="my-4 grid text-center lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"
       >
         <a
-          href="#"
+          href="https://nitdgp-hacks.devfolio.co/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
--- app/tracks/page.tsx
+++ app/tracks/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { motion } from "framer-motion";
+import IsteLogo from "@/assets/white iste logo.png";
 import RightArrow from "@/components/RightArrow";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 interface Event {
   title: string;
@@ -45,24 +48,44 @@ const Events: Array<Event> = [
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-[url('https://assets.squarespace.com/universal/images-v6/parking-page/black-grid-1500w.avif')]">
-      <div className="z-10 max-w-6xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="md:fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 p-2 md:pb-6 md:pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <Link href="/">NITDGP Hacks 1.0</Link>
-        </p>
-        <div className="md:fixed bottom-0 left-0 flex h-12 md:h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-0 md:p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.istenitdgp.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By
-            <div className="font-bold">
-              ISTE Students&apos; Chapter NIT Durgapur
-            </div>
+      <nav className="rounded-lg w-full bg-opacity-50 bg-black text-white py-4 px-8 flex justify-between items-center">
+        <Image
+          alt="Logo"
+          className="h-12"
+          height="50"
+          src={IsteLogo}
+          style={{
+            aspectRatio: "50/50",
+            objectFit: "cover",
+          }}
+          width="50"
+        />
+        <div className="hidden md:flex items-center space-x-4 lg:space-x-8">
+          <a className="hover:text-blue-500" href="/">
+            Home
           </a>
+          <a className="hover:text-blue-500" href="/tracks">
+            Tracks
+          </a>
+          <a
+            className="hover:text-blue-500"
+            href="https://www.istenitdgp.com/events"
+          >
+            Events
+          </a>
+          <a
+            className="hover:text-blue-500"
+            href="https://www.istenitdgp.com/teams"
+          >
+            Team
+          </a>
+          <Link href="https://nitdgp-hacks.devfolio.co/">
+            <Button className="bg-blue-500 hover:bg-blue-600 text-white">
+              Register
+            </Button>
+          </Link>
         </div>
-      </div>
+      </nav>
 
       <motion.div
         initial={{ opacity: 0, y: 40 }}
@@ -115,7 +138,7 @@ export default function Home() {
         className="my-24 grid text-center lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"
       >
         <a
-          href="#"
+          href="https://nitdgp-hacks.devfolio.co/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -134,8 +157,6 @@ export default function Home() {
         <a
           href="/tracks"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Tracks{" "}
